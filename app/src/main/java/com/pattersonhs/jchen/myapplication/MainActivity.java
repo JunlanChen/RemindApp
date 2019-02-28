4ea2e71bca0f9599fb0c668148675c0c929aef95
JunlanChen <junlanchen0128@gmail.com>
2019-02-28 14:13:04 -0500

    layout of the app is done, working on opening the alarm app

---
+++ app/src/main/java/com/pattersonhs/jchen/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.pattersonhs.jchen.myapplication;
+
+import android.content.Intent;
+import android.provider.AlarmClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.AlarmManager;
+
+import static java.lang.Integer.parseInt;
+
+public class MainActivity extends AppCompatActivity {
+    EditText wakeUpTimeEdit;
+    EditText travelTimeEdit;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wakeUpTimeEdit = (EditText) findViewById(R.id.wakeUpTime);
+        travelTimeEdit = (EditText) findViewById(R.id.travelTime);
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        //int time = parseInt(wakeUpTimeEdit.getText().toString());
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+                i.putExtra(AlarmClock.EXTRA_MESSAGE,"NEW ALARM");
+                i.putExtra(AlarmClock.EXTRA_HOUR,10);
+                i.putExtra(AlarmClock.EXTRA_MINUTES,45);
+                startActivity(i);
+            }
+        });
+    }
+
+}
